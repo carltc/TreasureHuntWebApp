26493926b66b8c6bcf6aab0cb31558c1f412ee82
Carl Chittenden <carltc@hotmail.co.uk>
2018-10-16 18:14:38 +0100

    Added connection string for online SQL server.

--- TreasureHuntWebApp/Startup.cs
+++ TreasureHuntWebApp/Startup.cs
@@ -37,7 +37,7 @@ namespace TreasureHuntWebApp
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<TreasureHuntWebAppContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("TreasureHuntWebAppContext")));
+                    options.UseSqlServer(Configuration.GetConnectionString("Mashdown_Questions")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
