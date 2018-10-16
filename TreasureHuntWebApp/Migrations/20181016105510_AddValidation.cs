2c8468af096c5e573b2c2f48722dfadfca96d26c
Carl Chittenden <carltc@hotmail.co.uk>
2018-10-16 15:14:28 +0100

    New migrations for validations on text boxes. But these didn't work so removed for now.

---
+++ TreasureHuntWebApp/Migrations/20181016105510_AddValidation.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TreasureHuntWebApp.Migrations
+{
+    public partial class AddValidation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Question",
+                maxLength: 60,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Question",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Answer",
+                table: "Question",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Question",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Question",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Answer",
+                table: "Question",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+        }
+    }
+}
---
+++ TreasureHuntWebApp/Migrations/20181016114426_RemoveValidation.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TreasureHuntWebApp.Migrations
+{
+    public partial class RemoveValidation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Question",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Question",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Answer",
+                table: "Question",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Question",
+                maxLength: 60,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Question",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Answer",
+                table: "Question",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
