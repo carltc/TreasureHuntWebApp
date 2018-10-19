4870c8979d565364cd4bd3cf2709859e7d58eee4
Carl Chittenden <carltc@hotmail.co.uk>
2018-10-19 12:33:17 +0100

    fixed issue with partial words getting answer right.

--- TreasureHuntWebApp/Pages/AskResponses/Index.cshtml.cs
+++ TreasureHuntWebApp/Pages/AskResponses/Index.cshtml.cs
@@ -27,45 +27,45 @@ namespace TreasureHuntWebApp.Pages.AskResponses
         {
             Question = ask;
 
-            var responses = from r in _context.AskResponse
+            var completeResponses = from r in _context.AskResponse
                          select r;
+
+            var responses = completeResponses;
 
             CultureInfo culture = new CultureInfo("en-GB");
 
             if (!String.IsNullOrEmpty(ask))
             {
-                responses = responses.Where(r => r.AskType != 3);
-                responses = responses.Where(r => r.Question.Contains(ask));
+                //responses = completeResponses.Where(r => r.AskType == 1);
+                //responses = responses.Where(r => r.Question.Contains(ask));
+
+                //if (!responses.Any())
+                //{
+                responses = completeResponses.Where(r => r.AskType == 1);
+                responses = responses.Where(r => culture.CompareInfo.IndexOf(ask, r.Question, CompareOptions.IgnoreCase) >= 0);
 
                 if (!responses.Any())
                 {
+                    _context.AskResponse.AddRange(
+                        new AskResponse
+                        {
+                            Question = ask,
+                            Response = "",
+                            AskDate = DateTime.Now,
+                            AskType = 3
+                        }
+                    );
+                    _context.SaveChanges();
+
                     responses = from r in _context.AskResponse
-                                select r;
-                    responses = responses.Where(r => r.AskType == 1);
-                    responses = responses.Where(r => culture.CompareInfo.IndexOf(ask, r.Question, CompareOptions.IgnoreCase) >= 0);
-
-                    if (!responses.Any())
-                    {
-                        _context.AskResponse.AddRange(
-                            new AskResponse
-                            {
-                                Question = ask,
-                                Response = "",
-                                AskDate = DateTime.Now,
-                                AskType = 3
-                            }
-                        );
-                        _context.SaveChanges();
-
-                        responses = from r in _context.AskResponse
-                                        select r;
-                        responses = responses.Where(r => r.AskType == 2);
-                    }
+                                    select r;
+                    responses = responses.Where(r => r.AskType == 2);
                 }
+                //}
             }
             else
             {
-                responses = responses.Where(r => r.Question.Contains("sfjahflasugdfieafhlaKBFsiegfiabsfABFIAgfs"));
+                responses = completeResponses.Where(r => r.Question.Contains("sfjahflasugdfieafhlaKBFsiegfiabsfABFIAgfs"));
             }
 
             AskResponse = await responses.ToListAsync();
