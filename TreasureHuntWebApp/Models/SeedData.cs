49bca59354cc62e2b4ea421148b452425af6f5cf
Carl Chittenden <carltc@hotmail.co.uk>
2018-11-01 21:47:41 +0000

    Medieval Dungeon story half written.

--- TreasureHuntWebApp/Models/SeedData.cs
+++ TreasureHuntWebApp/Models/SeedData.cs
@@ -370,6 +370,7 @@ namespace TreasureHuntWebApp.Models
                             dungeon.Name = "Pit";
                             dungeon.Storyline = "You fell in a pit.";
                             dungeon.ItemID = 0;
+                            dungeon.Guidebook = "The master of the castle loved practical jokes and installed many pits into which his guests could fall. All the pits connected to the same underground network and led back to the entrance hall.";
 
                             if (i > 1){ dungeon.WestID = westID; }
                             if (i < 6) { dungeon.EastID = eastID; }
@@ -380,77 +381,91 @@ namespace TreasureHuntWebApp.Models
                             {
                                 dungeon.Name = "Start";
                                 dungeon.Storyline = "You wake up dazed and confused on a cold, hard floor. You look around and see a dark dingy room, lit only by torches. There are 2 old wooden doors ahead of you which look unlocked...";
+                                dungeon.Guidebook = "Welcome to the Castle Emon Guidebook. Throughout your tour this book will attempt to aid you in the navigation of the castle. You can read about each room by turning to the relevant page.";
                             }
                             else if (id == 2)
                             {
                                 dungeon.Name = "Sleepy Head";
-                                dungeon.Storyline = "Hurrrgggghhhhh! A loud grunt startles you as you enter the room. Running away from the sound into the room, you turn as the door bangs shut and the giant troll that was sleeping behind it stands up. Not fazed by this, you take an aggressive stance and prepare to attack the troll.";
+                                dungeon.Storyline = "Hurrrgggghhhhh! A loud grunt startles you as you enter the room. Running away from the sound into the room, you turn as the door bangs shut and the giant troll that was sleeping behind it stands up. On it's head is a large wooden crown with the words 'King Sower' written crudely. It appears to have been nailed to it's skull. Not fazed by this, you take an aggressive stance and prepare to attack...";
                                 dungeon.ItemID = 1; // monster
+                                dungeon.Guidebook = "In this room, Lord Emon locked up the tyrant troll king, King Sower.";
                             }
                             else if (id == 3)
                             {
-                                dungeon.Name = "Empty Room";
-                                dungeon.Storyline = "You enter a dark room. You look around and notice more wooden doors...";
+                                dungeon.Name = "The Dungeon";
+                                dungeon.Storyline = "Moving into the room you step in a puddle and the sound echos off the walls. On one side are a series of cells. To the west is a large door with the words 'King Sower' written on. To the east lies a smaller door...";
+                                dungeon.Guidebook = "Lord Emon had a dark sense of humour and designed his castle such that you had to walk through the dungeons to reach the main castle complex. This is also where he imprisoned the troll leader, King Sower.";
                             }
                             else if (id == 4)
                             {
-                                dungeon.Name = "Empty Room";
-                                dungeon.Storyline = "You enter a dark room. You look around and notice more wooden doors...";
+                                dungeon.Name = "Dungeon Corridor";
+                                dungeon.Storyline = "You emerge into a long corridor. It runs from east to west with small doors at either end. In the middle there is another small door and as you walk up to it you notice scratches and patches of blood.";
+                                dungeon.Guidebook = "This corridor was a simple route past the prank pit to the north. By heading west you are able to visit the castle dungeons and to the east are the castle main living quarters.";
                             }
                             else if (id == 5)
                             {
-                                dungeon.Name = "Empty Room";
-                                dungeon.Storyline = "You enter a dark room. You look around and notice more wooden doors...";
+                                dungeon.Name = "Torture Chamber";
+                                dungeon.Storyline = "In the middle of the room is a strange contraption. Looking like a cross between a chair and a candelabra, you quickly realise it is some form of torture device. At this point you notice the rest of the devices around the edges. You hurry to get out of here and into another room.";
+                                dungeon.Guidebook = "Always the comedian, Lord Emon created this fake torture chamber to scare his guests. FUN FACT: The central torture device was actually made out of a candelabra and a small chair. This room connected the castle living quarters to the north with the dungeons to the west.";
                             }
                             else if (id == 7)
                             {
-                                dungeon.Name = "Empty Room";
-                                dungeon.Storyline = "You enter a dark room. You look around and notice more wooden doors...";
+                                dungeon.Name = "Stone Room";
+                                dungeon.Storyline = "You enter a dark room. It is made entirely out of stone. You look around and notice more wooden doors...";
+                                dungeon.Guidebook = "The stone room was used to entertain guests. The smell of food wafted in through the north door and guests were often seen exploring through the east door.";
                             }
                             else if (id == 9)
                             {
-                                dungeon.Name = "Empty Room";
-                                dungeon.Storyline = "You enter a dark room. You look around and notice more wooden doors...";
+                                dungeon.Name = "Dungeon Corridor";
+                                dungeon.Storyline = "You walk down a long corridor. To the south a sign above the door reads 'Dungeons' and to the north you see 'Armory'. In the middle on either side are 2 smaller doors.";
+                                dungeon.Guidebook = "In an attempt to prevent escaping prisoners from leaving the 2 main doors of the corridor were labelled in order to deter them from escaping through these. Therefore the prisoners would be more likely to try the smaller doors in the middle which both led to pits.";
                             }
                             else if (id == 11)
                             {
-                                dungeon.Name = "Empty Room";
-                                dungeon.Storyline = "You enter a dark room. You look around and notice more wooden doors...";
+                                dungeon.Name = "Guard Room";
+                                dungeon.Storyline = "You peer around the door and immediately pull back, out of sight. All around the room were a large number of dark figures. One must clearly have seen you or the door move but you hear no movement or voices. You peer around again, more cautiously this time and realise they are all empty suits of armour. On further inspection of the room you see the easterly door looks well used and the north and west appear to have been hardly touched.";
+                                dungeon.Guidebook = "This is the Guard Room where Lord Emon's Banen Guards lived. They spent most of their time polishing armour to give it a golden, almost yellow, glint. They never used the north or west doors as Lord Emon had put traps behind them.";
                             }
                             else if (id == 12)
                             {
-                                dungeon.Name = "Empty Room";
-                                dungeon.Storyline = "You enter a dark room. You look around and notice more wooden doors...";
+                                dungeon.Name = "Entrance Hall";
+                                dungeon.Storyline = "A long shaft of light pierces the eary blackness of the room. A large staircase leads up to a door at the north of the room. Suspended on all the walls are various sections of trees and plants, presented like animal heads, stuck on black plaques which appear to be moving. On closer examination the plaques are covered in a thick layer of flies and you realise they are made out of flesh.";
+                                dungeon.Guidebook = "This room was the main entrance to the living quarters of the castle. It was designed to be lavish and show any guests the wealth that Lord Emon had amassed through his travels to North Eastern India. His personal chambers are to the north and he sent his less favoured subjects to sleep behind the south door.";
                             }
                             else if (id == 13)
                             {
-                                dungeon.Name = "Empty Room";
-                                dungeon.Storyline = "You enter a dark room. You look around and notice more wooden doors...";
+                                dungeon.Name = "Dining Room";
+                                dungeon.Storyline = "This room has more light. It allows you to see what was once a fine banquet hall. No however it lies in disarray and there is a thick layer of dust across everything. A faint wiff of food comes from a northerly direction.";
+                                dungeon.Guidebook = "Dinners and banquets were frequent at Castle Emon and most took place in this very room. The food would be delivered from the north door and guests would lounge around eating their fill before emptying themselves through the east door.";
                             }
                             else if (id == 15)
                             {
-                                dungeon.Name = "Empty Room";
-                                dungeon.Storyline = "You enter a dark room. You look around and notice more wooden doors...";
+                                dungeon.Name = "Armory Storeroom";
+                                dungeon.Storyline = "This room looks like it has been raided. Most of the cupboard that once lined the walls lay knocked over in the middle. Sword racks and various tools lie strewn around the room. No weapons here anymore.";
+                                dungeon.Guidebook = "Surplus weapons and armour were stored in this room. The best pieces could be found in the armoury and trophy room to the north. It was also a room which hid the entrance to the dungeons and then onto the living quarters.";
                             }
                             else if (id == 18)
                             {
-                                dungeon.Name = "Empty Room";
-                                dungeon.Storyline = "You enter a dark room. You look around and notice more wooden doors...";
+                                dungeon.Name = "The Lounge";
+                                dungeon.Storyline = "As soon as you open the door there is an explosion of light. You retreat slightly as your eyes get used to the new room. Squinting, you look around a large room lined with plush sofas and various pieces of furniture. The light is streaming through 2 large windows in the western wall. The room isn't clean but someone has definitely been using this room and recently too by the lack of dust around the northern door.";
+                                dungeon.Guidebook = "The lounge was were Lord Emon relaxed. He designed it to be so comfortable that he once said 'If I ever lost my mind, I like to think that I would still use this room due to the effect it has on me, regardless of my sanity'. The only room that was allowed windows, 2 large opening were carved into the west wall.";
                             }
                             else if (id == 19)
                             {
-                                dungeon.Name = "Empty Room";
-                                dungeon.Storyline = "You enter a dark room. You look around and notice more wooden doors...";
+                                dungeon.Name = "Kitchen";
+                                dungeon.Storyline = "AS you open the door you are hit by a sea of different smells. Around the dimly lit room there are piles of rotting food and the resemblance of a kitchen. You see a glimmer of light coming from a door to the north.";
+                                dungeon.Guidebook = "The kitchen was Lord Emon's pride and joy. He loved cooking everything and anything. He kept a supply closet near to the north, through which his private study lay in case he needed to check some recipes quickly. It also connected the guest areas to the rest of the castle through the east doorway.";
                             }
                             else if (id == 20)
                             {
-                                dungeon.Name = "Empty Room";
+                                dungeon.Name = "Maze Room";
                                 dungeon.Storyline = "You enter a dark room. You look around and notice more wooden doors...";
                             }
                             else if (id == 21)
                             {
-                                dungeon.Name = "Empty Room";
+                                dungeon.Name = "Armory";
                                 dungeon.Storyline = "You enter a dark room. You look around and notice more wooden doors...";
+                                dungeon.Guidebook = "Weapons and Armour fascinated Lord Emon. He always brought them back from travels and stored them in his armoury. When it got too full he put the smaller pieces in the storeroom to the south. His best pieces were reserved for the trophy room which can be found to the north.";
                             }
                             else if (id == 23)
                             {
@@ -472,7 +487,7 @@ namespace TreasureHuntWebApp.Models
                             }
                             else if (id == 27)
                             {
-                                dungeon.Name = "Empty Room";
+                                dungeon.Name = "Trophy Room";
                                 dungeon.Storyline = "You enter a dark room. You look around and notice more wooden doors...";
                             }
                             else if (id == 30)
