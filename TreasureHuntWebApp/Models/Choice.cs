b65b3b2928eb4f3539a4f0680d2866acac29d694
Carl Chittenden <carltc@hotmail.co.uk>
2018-11-08 16:53:30 +0000

    Added choices hunt

---
+++ TreasureHuntWebApp/Models/Choice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TreasureHuntWebApp.Models
+{
+    public class Choice
+    {
+        public int ID { get; set; }
+
+        public string Question { get; set; }
+
+        public string Choice1 { get; set; }
+
+        public string Result1 { get; set; }
+
+        public string Choice2 { get; set; }
+
+        public string Result2 { get; set; }
+
+        public string Choice3 { get; set; }
+
+        public string Result3 { get; set; }
+
+        public int CorrectAnswer { get; set; }
+    }
+}
