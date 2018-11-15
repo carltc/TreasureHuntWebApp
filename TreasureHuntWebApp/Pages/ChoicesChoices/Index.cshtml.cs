2a4b34c4ec44b927f53b0b6f174420d83e56da3b
Carl Chittenden <carltc@hotmail.co.uk>
2018-11-15 14:45:33 +0000

    Choices labyrinth working with question and answers. No scoring yet.

--- TreasureHuntWebApp/Pages/ChoicesChoices/Index.cshtml.cs
+++ TreasureHuntWebApp/Pages/ChoicesChoices/Index.cshtml.cs
@@ -6,16 +6,18 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using TreasureHuntWebApp.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace TreasureHuntWebApp.Pages.ChoicesChoices
 {
     public class IndexModel : PageModel
     {
-        public IActionResult OnPost(string button)
+        public IActionResult OnPost(string button, string PlayerName)
         {
             if (button == "Play Game")
             {
-                return RedirectToPage("./TheLabyrinth");
+                HttpContext.Session.SetString("PlayerName", PlayerName);
+                return Redirect("./ChoicesChoices/TheLabyrinth?cID=1");
             }
             return Page();
         }
--- TreasureHuntWebApp/Pages/ChoicesChoices/TheLabyrinth.cshtml.cs
+++ TreasureHuntWebApp/Pages/ChoicesChoices/TheLabyrinth.cshtml.cs
@@ -20,20 +20,54 @@ namespace TreasureHuntWebApp.Pages.ChoicesChoices
         }
 
         public IList<Choice> Choice { get; set; }
+        public String PreviousResult { get; set; }
+        public int CurrentChoice = 0;
 
-        public async Task OnGetAsync(int? choiceID, int? fight)
+        public async Task OnGetAsync(int? cID, int? pR)
         {
-            var choices = from choice in _context.Choice
-                           where choice.ID == choiceID
-                           select choice;
-            Choice = await choices.ToListAsync();
+            if (!String.IsNullOrEmpty(HttpContext.Session.GetString("PlayerName")))
+            {
+                if (cID.HasValue)
+                {
+                    int choiceID = (int)cID;
+                    var choices = from choice in _context.Choice
+                                  where choice.ID == choiceID
+                                  select choice;
+                    Choice = await choices.ToListAsync();
+
+                    CurrentChoice = choiceID;
+                
+                    if (pR.HasValue && choiceID > 1)
+                    {
+                        var previousResults = from choice in _context.Choice
+                                              where choice.ID == choiceID - 1
+                                              select choice;
+                        var tempPR = await choices.ToListAsync();
+                        if (pR == 1) { PreviousResult = tempPR[0].Result1; }
+                        else if (pR == 2) { PreviousResult = tempPR[0].Result2; }
+                        else if (pR == 3) { PreviousResult = tempPR[0].Result3; }
+                        else { PreviousResult = ""; }
+                    }
+                    else
+                    {
+                        PreviousResult = "";
+                    }
+                }
+                else
+                {
+                    Choice = new List<Choice> { };
+                    Choice.Add(new Choice { ID = 0, Choice1 = "", Choice2 = "", Choice3 = "", CorrectAnswer = 0, Question = "", Result1 = "", Result2 = "", Result3 = ""});
+                    PreviousResult = "";
+                }
+            }
         }
 
-        public IActionResult OnPost(string door)
+        public IActionResult OnPost(string door, int choice)
         {
             if (!String.IsNullOrEmpty(door))
             {
-                return Redirect("./TheLabyrinth?choiceID=" + door.ToString());
+                choice++;
+                return Redirect("./TheLabyrinth?cID=" + choice.ToString() + "&pR=" + door);
             }
             return Page();
         }
